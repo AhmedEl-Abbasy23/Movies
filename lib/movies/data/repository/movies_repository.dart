c8e57c15eac3cdd49b43e6f1c1409751cda43111
AHMED EL-ABBASY <ahmed.elabbasy23@gmail.com>
2022-08-21 17:32:24 +0200

    first commit

---
+++ lib/movies/data/repository/movies_repository.dart
@@ -0,0 +1,67 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies_app/core/error/exceptions.dart';
+import 'package:movies_app/core/error/failure.dart';
+import 'package:movies_app/movies/data/datasource/movie_remote_datasource.dart';
+import 'package:movies_app/movies/domain/entities/movie.dart';
+import 'package:movies_app/movies/domain/entities/movie_detail.dart';
+import 'package:movies_app/movies/domain/entities/recommendation_movies.dart';
+import 'package:movies_app/movies/domain/repository/base_movies_repository.dart';
+import 'package:movies_app/movies/domain/usecases/get_movie_details_usecase.dart';
+import 'package:movies_app/movies/domain/usecases/get_recommendation_movies_usecase.dart';
+
+// concrete implementation
+class MoviesRepository extends BaseMoviesRepository {
+  final BaseMovieRemoteDataSource baseMovieRemoteDataSource;
+
+  MoviesRepository(this.baseMovieRemoteDataSource);
+
+  @override
+  Future<Either<Failure, List<Movie>>> getNowPlayingMovies() async {
+    final result = await baseMovieRemoteDataSource.getNowPlayingMovies();
+    try {
+      return Right(result);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(failure.errorMessageModel.statusMessage));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Movie>>> getPopularMovies() async {
+    final result = await baseMovieRemoteDataSource.getPopularMovies();
+    try {
+      return Right(result);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(failure.errorMessageModel.statusMessage));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Movie>>> getTopRatedMovies() async {
+    final result = await baseMovieRemoteDataSource.getTopRatedMovies();
+    try {
+      return Right(result);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(failure.errorMessageModel.statusMessage));
+    }
+  }
+
+  @override
+  Future<Either<Failure, MovieDetail>> getMovieDetails(MovieDetailsParameters parameters) async{
+    final result = await baseMovieRemoteDataSource.getMovieDetails(parameters);
+    try {
+      return Right(result);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(failure.errorMessageModel.statusMessage));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<RecommendationMovies>>> getRecommendationMovies(RecommendationMoviesParameters parameters) async{
+    final result = await baseMovieRemoteDataSource.getRecommendationMovies(parameters);
+    try {
+      return Right(result);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(failure.errorMessageModel.statusMessage));
+    }
+  }
+}
